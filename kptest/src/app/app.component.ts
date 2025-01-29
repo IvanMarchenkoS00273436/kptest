41ca37a7afcdcda6bca75060bb34c40e13c43d11
IvanMarchenkoS00273436 <S00273436@atu.ie>
2025-01-29 12:06:48 +0000

    To do list

--- kptest/src/app/app.component.ts
+++ kptest/src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'kptest';
+  studentName = "Ivan";
+  studentLastName = "Marchenko";
+  studentId = "S00273436";
+
+  todoTitle = "To do list";
+
+  public thingsToDo: string[] = [
+    "Learn JavaScript",
+    "Learn Angular",
+    "Learn Redux"
+  ]
+
+  public thingsCompleted: string[] = [];
+
+  summary(): string {
+    return `${this.thingsToDo.length} to do / ${this.thingsCompleted.length} done`;
+  };
 }
